e46f74818d2ba4d678c1af29cc758865372e036f
Shutong Jia <tongjia@berkeley.edu>
2025-08-12 00:48:16 +0000

    Part 1: filter movies by MPAA ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ApplicationRecord
+  RATINGS = %w[G PG PG-13 R NC-17].freeze
+
+  def self.all_ratings
+    RATINGS
+  end
+
+  def self.with_ratings(ratings_list)
+    return all if ratings_list.blank?
+    where('upper(rating) IN (?)', ratings_list.map(&:upcase))
+  end
+
+  def self.sorted(scope, sort_by)
+    return scope if sort_by.blank? || !%w[title release_date].include?(sort_by)
+    scope.order(sort_by => :asc)
+  end
 end
