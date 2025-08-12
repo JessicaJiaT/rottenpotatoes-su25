43c83c1096b5ee27e4268b05b98d79111d9135aa
Shutong Jia <tongjia@berkeley.edu>
2025-08-12 04:09:43 +0000

    Part 1: filter by ratings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,16 @@
-class MoviesController < ApplicationController
-  def index
-    # 获取所有电影的评级（去重、去空值、按字母排序）
-    @all_ratings = Movie.distinct.pluck(:rating).compact.sort
+def index
+  @all_ratings = Movie.all_ratings
 
-    # 如果 params[:ratings] 存在，使用它；否则默认选中所有评级
-    if params[:ratings]
-      # 如果是 Hash（或 ActionController::Parameters），取 key；否则直接用数组
-      @selected_ratings = params[:ratings].is_a?(Hash) ? params[:ratings].keys : params[:ratings]
-    else
-      @selected_ratings = @all_ratings
-    end
-
-    # 获取排序参数（可能是 "title" 或 "release_date"）
-    @sort = params[:sort]
-
-    # 先按评级过滤电影
-    @movies = Movie.where(rating: @selected_ratings)
-
-    # 如果排序字段有效，则按该字段排序
-    if %w[title release_date].include?(@sort)
-      @movies = @movies.order(@sort)
-    end
+  if params[:ratings].present?
+    @ratings_to_show = params[:ratings].keys
+  else
+    @ratings_to_show = @all_ratings
   end
+
+  @sort_by = params[:sort_by] 
+  scope   = Movie.with_ratings(@ratings_to_show)
+  @movies = Movie.sorted(scope, @sort_by)
+
+
+  @ratings_hash = @ratings_to_show.product(['1']).to_h
 end
