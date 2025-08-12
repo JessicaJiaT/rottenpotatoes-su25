ac1617a64edcb6e98c678ba6ff35bca433cda770
Shutong Jia <tongjia@berkeley.edu>
2025-08-12 05:19:00 +0000

    Fix: wrap index in MoviesController class; header links in view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,31 +1,35 @@
+class MoviesController < ApplicationController
+  def index
+    @all_ratings = Movie.all_ratings
 
-def index
-  @all_ratings = Movie.all_ratings
+    if params[:ratings].present?
+      @ratings_to_show    = params[:ratings].keys
+      session[:ratings_h] = params[:ratings]
+    elsif session[:ratings_h].present?
+      @ratings_to_show = session[:ratings_h].keys
+      params[:ratings] = session[:ratings_h]
+    else
+      @ratings_to_show = @all_ratings
+    end
 
-  # ---- ratings ----
-  if params[:ratings].present?
-    @ratings_to_show    = params[:ratings].keys
-    session[:ratings_h] = params[:ratings]
-  elsif session[:ratings_h].present?
-    @ratings_to_show = session[:ratings_h].keys
-    params[:ratings] = session[:ratings_h] # 回填，便于视图/链接一致
-  else
-    @ratings_to_show = @all_ratings
+    if params[:sort_by].present?
+      @sort_by = params[:sort_by]
+      session[:sort_by] = @sort_by
+    elsif session[:sort_by].present?
+      @sort_by = session[:sort_by]
+      params[:sort_by] = @sort_by
+    else
+      @sort_by = nil
+    end
+
+    scope   = Movie.with_ratings(@ratings_to_show)
+    @movies = Movie.sorted(scope, @sort_by)
+
+    @ratings_hash = @ratings_to_show.product(['1']).to_h
   end
 
-  # ---- sort_by ----
-  if params[:sort_by].present?
-    @sort_by = params[:sort_by]
-    session[:sort_by] = @sort_by
-  elsif session[:sort_by].present?
-    @sort_by = session[:sort_by]
-    params[:sort_by] = @sort_by
-  else
-    @sort_by = nil
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date, :description)
   end
-
-  scope   = Movie.with_ratings(@ratings_to_show)
-  @movies = Movie.sorted(scope, @sort_by)
-
-  @ratings_hash = @ratings_to_show.product(['1']).to_h
 end
